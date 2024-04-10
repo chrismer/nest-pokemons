700d9152fd831500c664f45e5d42f9e5097fee92
u608014 <cmereles@teco.com.ar>
2024-04-10 08:01:53 -0300

    preparar mongodb y envs

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { SeedController } from './seed.controller';
 import { SeedService } from './seed.service';
+import { PokemonModule } from '../pokemon/pokemon.module';
+import { CommonModule } from '../common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
+  imports: [PokemonModule, CommonModule],
 })
 export class SeedModule {}
